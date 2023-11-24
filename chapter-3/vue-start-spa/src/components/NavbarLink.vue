2d9716dd3cb18cf2b0d6a511644efed9210e0c80
Dragan Djordjevic <GaGzO78@gmail.com>
2023-11-24 02:28:15 +0100

    chapter 3 done

---
+++ chapter-3/vue-start-spa/src/components/NavbarLink.vue
@@ -0,0 +1,26 @@
+<template>
+    <a 
+    :href="page.link.url" class="nav-link" 
+    :class="activeClasses" 
+    aria-current="page">{{ page.link.text }}
+    </a>
+</template>
+
+<script>
+export default{
+    props: ['page','isActive'],
+    computed: {
+        activeClasses(){
+            return{
+                active: this.isActive,
+                emphasize: this.isActive
+            };
+        }
+    }
+}
+</script>
+<style scoped>
+.emphasize{
+    text-decoration: underline !important;
+}
+</style>
